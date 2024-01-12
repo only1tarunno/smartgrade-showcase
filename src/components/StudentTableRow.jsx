a8f7fe700b58a595ba7bf5083847d73637caa57e
only1tarunno <tarunno281@gmail.com>
2024-01-12 11:17:58 +0600

    student table create

--- src/components/StudentTableRow.jsx
+++ src/components/StudentTableRow.jsx
@@ -1,26 +1,26 @@
-// /* eslint-disable react/prop-types */
+/* eslint-disable react/prop-types */
 
-// const StudentTableRow = ({ student }) => {
-//   const { id, name, score, percentage, img } = student || {};
-//   return (
-//     <tr className="border-b border-[#7ECEB529]">
-//       <td className="p-5 text-sm md:text-xl">{id}</td>
-//       <td className="p-5 text-sm md:text-xl">
-//         <div className="flex space-x-3 items-center">
-//           <img
-//             className="w-8 h-8 rounded-[50%]"
-//             src={img}
-//             width="32"
-//             height="32"
-//             alt="John Smith"
-//           />
-//           <span className="whitespace-nowrap">{name}</span>
-//         </div>
-//       </td>
-//       <td className="p-5 text-sm md:text-xl text-center">{score}</td>
-//       <td className="p-5 text-sm md:text-xl text-center">{percentage}%</td>
-//     </tr>
-//   );
-// };
+const StudentTableRow = ({ student }) => {
+  const { id, name, score, percentage, img } = student || {};
+  return (
+    <tr className="border-b border-[#7ECEB529]">
+      <td className="p-5 text-sm md:text-xl">{id}</td>
+      <td className="p-5 text-sm md:text-xl">
+        <div className="flex space-x-3 items-center">
+          <img
+            className="w-8 h-8 rounded-[50%]"
+            src={img}
+            width="32"
+            height="32"
+            alt="John Smith"
+          />
+          <span className="whitespace-nowrap">{name}</span>
+        </div>
+      </td>
+      <td className="p-5 text-sm md:text-xl text-center">{score}</td>
+      <td className="p-5 text-sm md:text-xl text-center">{percentage}%</td>
+    </tr>
+  );
+};
 
-// export default StudentTableRow;
+export default StudentTableRow;
